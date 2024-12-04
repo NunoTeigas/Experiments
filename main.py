7f7e001766c71f9d6b6315226a5f982dc5a52c49
NunoTeigas <157012395+NunoTeigas@users.noreply.github.com>
2024-12-04 16:21:58 +0000

    Update

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyautogui
+"""
+----------------------------------------------------------------------------------
+>>>>>>>>>>>>>>>>>>>>> CONSTANTS need to be updated by user   <<<<<<<<<<<<<<<<<<<<<
+----------------------------------------------------------------------------------
+"""
+#Constants
+MINUTES = 1 #user input minutes for timer countdown
+INTERVAL = 10 #user input for interval in seconds to be notified and for the loop to perform
+
+#variables
+timer = int(MINUTES * 60 / INTERVAL)
+clicks = 0
+time_left = (MINUTES * 60) - (clicks * INTERVAL)
+print(f"You have {time_left} seconds remaining.")
+print(timer)
+
+for i in range(timer):
+    pyautogui.sleep(INTERVAL) # same for ALL
+    pyautogui.click((1274, 545), duration =0.1) #only for the 1st time
+    clicks += 1
+    time_left = (MINUTES * 60) - (clicks * INTERVAL)
+    print(f"You have {time_left} seconds remaining.")  #in seconds
+
+
---
+++ readme.rst
@@ -0,0 +1,17 @@
+Welcome to trick the boss clicker
+*********************************
+
+Instructions to use
+-------------------
+
+The CONSTANTS below require a user input.
+
+MINUTES: for how long the loop will execute.
+INTERVAL: frequency of the notification of how many seconds left in the timer.
+
+MINUTES = 1
+INTERVAL = 10
+
+This script uses the pyautogui module.
+
+Since we have a mouse replicator loop, there is a failsafe to stop and exit the program: move the cursor to the top left corner of the screen (coordinates x=0, y=0).
